faa042375cacfa3fd571e19fb773007de6b31b5b
shocyu <sanokebab@gmail.com>
2015-09-25 11:43:52 +0000

    Finish user registration

--- my_blog/config/routes.rb
+++ my_blog/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   get "users/show"
   devise_for :users
   resources :users, only: [:show]
